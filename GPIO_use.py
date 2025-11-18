4f690a36ead041154345fdb08774b784f804066b
UtpalKuma-r <utpal@placeholder.com>
2025-11-18 22:24:23 +0530

    added infinite loop

--- GPIO_use.py
+++ GPIO_use.py
@@ -5,17 +5,18 @@ import time
 h = lgpio.gpiochip_open(0)
 
 # Claim a pin as output (e.g., pin 17)
-lgpio.gpio_claim_output(h, 17)
+lgpio.gpio_claim_output(h, 14)
 
+while True:
 # Write a high value
-lgpio.gpio_write(h, 17, 1)
-time.sleep(1)
+    lgpio.gpio_write(h, 14, 1)
+    time.sleep(1)
 
-# Write a low value
-lgpio.gpio_write(h, 17, 0)
+    # Write a low value
+    lgpio.gpio_write(h, 14, 0)
 
 # Release the pin
-lgpio.gpio_free(h, 17)
+lgpio.gpio_free(h, 14)
 
 # Close the GPIO chip
 lgpio.gpiochip_close(h)
